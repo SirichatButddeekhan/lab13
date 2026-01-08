845db690169702ce030b9001bba93dcb30c6d1f5
sirichat <sirichatcnx60@gmail.com>
2026-01-09 00:03:44 +0700

    Complete lab13_1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,46 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double src1[],int N,double dest[]){
+
+    double sum=0;
+    for(int i=0; i<N ;i++){
+        sum += src1[i];
+    }
+    dest[0] = sum/N;
+
+    double def;
+    double newsum = 0 ;
+    for(int i=0;i<N;i++){
+        def = src1[i] - dest[0];
+        newsum += def * def;
+    }
+    dest[1] = pow(newsum / N,0.5);
+
+    double geomatric = 1;
+    for(int i=0;i<N;i++){
+        geomatric *= src1[i];
+    }
+    dest[2] = pow(geomatric,1.0/N);
+
+
+    double sum_harmonic;
+    for(int i=0; i<N ;i++){
+        sum_harmonic += 1/src1[i];
+    }
+    dest[3] = N/sum_harmonic;
+
+
+    double max = src1[0];
+    double min = src1[0];
+
+    for(int i=1;i < N; i++){
+        if(src1[i]> max)max = src1[i];
+        if(src1[i]< min)min = src1[i];
+    }
+
+    dest[4] = max;
+    dest[5] = min;
+
+}
