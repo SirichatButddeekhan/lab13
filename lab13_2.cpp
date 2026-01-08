2de3e9bb583d90dd46f54f8b02b3f035b7dfbef5
sirichat <sirichatcnx60@gmail.com>
2026-01-09 00:22:51 +0700

    Complete lab13_2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,30 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[][70], int s, int x, int y){
+    for(int i = 0; i < 30; i++){
+        for(int j = 0; j < 70; j++){
+            if (sqrt((i - x) * (i - x) + (j - y) * (j - y)) <= s - 1){
+                image[i][j] = true;
+            }
+        }
+    }
+}
+
+void showImage(const bool image[][70]){
+    for(int j = 0; j < 72; j++) cout << "-";
+    cout << "\n";
+
+    for(int i = 0; i < 30; i++){
+        cout << "|";
+        for(int j = 0; j < 70; j++){
+            cout << (image[i][j] ? "*" : " ");
+        }
+        cout << "|\n";
+    }
+
+    for(int j = 0; j < 72; j++) cout << "-";
+    cout << "\n";
+}
+
